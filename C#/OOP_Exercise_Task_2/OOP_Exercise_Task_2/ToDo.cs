0e92e6c89a02411a5c6273285c916e28194658db
Axtinator <m.missel@protonmail.com>
2023-09-24 20:10:37 +0200

    Updated Files

---
+++ C#/OOP_Exercise_Task_2/OOP_Exercise_Task_2/ToDo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise_Task_2
+{
+    internal class ToDo
+    {
+        private string _topic;
+        private string _assinged;
+        private string _note;
+        private DateTime _dateTime;
+        private bool _done;
+
+        public ToDo(string topic, string assinged, string note, DateTime dateTime, bool done) { 
+            _topic = topic;
+            _assinged = assinged;
+            _note = note;
+            _dateTime = dateTime;
+            _done = done;
+        }
+
+        public string Topic
+        {
+            get => _topic;
+            set => _topic = value;
+        }
+
+        public string Assinged
+        {
+            get => _assinged;
+            set => _assinged = value;
+        }
+
+        public string Note
+        {
+            get => _note;
+            set => _note = value;
+        }
+
+        public DateTime DateTime
+        {
+            get => _dateTime;
+            set => _dateTime = value;
+        }
+
+        public bool Done
+        {
+            get => _done; 
+            set => _done = value;
+        }
+    }
+}
---
+++ C#/OOP_Exercise_Task_3/OOP_Exercise_Task_3/insurance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise_Task_3
+{
+    internal class insurance
+    {
+        private string _manufacturer;
+        private string _modelType;
+        private string _manufacturingDate;
+        private float _strokeVolume;
+        private int _ps;
+        private int _kilometersPerYear;
+        private int 
+    }
+}
